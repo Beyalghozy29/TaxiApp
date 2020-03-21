09d72d149c709fc31868dd199644da7078317c8a
Beyalghozy29 <muhammad.2903@students.amikom.ac.id>
2020-03-21 21:38:04 +0700

    TaxiApp

---
+++ TaxiApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApp
+{
+    //method
+    public class Taxi
+    {
+        public string DriverName    { get; set; }
+        public bool OnDuty          { get; set; }
+        public int NumbOfPassenger  { get; set; }
+        public string status        { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name          : {0}", DriverName);
+            if (OnDuty == true)
+                 {
+                    status = "Yes";
+                 }
+            else
+                  {
+                    status = "No";
+                  }
+            Console.WriteLine("On Duty              : {0}", status);
+
+            Console.WriteLine("Number Of Passenger  : {0}\n", NumbOfPassenger);
+        }
+
+        public void PickUpPassenger()
+                {
+                     Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName);
+                }
+
+        public void DropOfPassenger()
+                  {
+                     Console.WriteLine("{0} Sedang Mengantar Penumpang", DriverName);
+                  }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumbOfPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOfPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
